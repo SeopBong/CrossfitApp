fd90364a5eced62f26545bd17d1b63e9774a304e
SeopBong <gkstjq309@gmail.com>
2023-02-03 19:30:20 +0900

    0203

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 
-
 function newPage() { 
 //window.location.href = 'C:\Users\hspar\Desktop\jsProject\myFirstProject\index1.html';
 window.open('C:\Users\hspar\Desktop\jsProject\myFirstProject\index1.html');
