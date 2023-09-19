b725aa99e5a2d03e0922db6297de6650436f166c
SeopBong <gkstjq309@gmail.com>
2023-09-19 18:07:06 +0900

    loginbtn

--- src/UI/NavBar.js
+++ src/UI/NavBar.js
@@ -5,6 +5,8 @@ import '../UI/NavBar.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
+
 
 
 
@@ -17,11 +19,13 @@ const NavBar = () => {
                 <Nav className="me-auto">
                     <Nav.Link href="#workout">WOD</Nav.Link>
                     <Nav.Link href="#Record">Record</Nav.Link>
-                    <Nav.Link href="#Data">Data</Nav.Link>
-                    <Nav.Link href="#Competition">Competition</Nav.Link>
+                    <Nav.Link href="#data">Data</Nav.Link>
+                    <Nav.Link href="#competition">Competition</Nav.Link>
                     <Nav.Link href="#Event">Event</Nav.Link>
-                    <Nav.Link href="#pricing">Ranking</Nav.Link>
+                    <Nav.Link href="#rank">Ranking</Nav.Link>
                 </Nav>
+                <Button variant="outline-light">Login</Button>{' '}
+
                 </Container>
       </Navbar>
         </div>
